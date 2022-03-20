b6310235650821910fbdaefb25af5f12d28bc94f
farazovo <farazovo@gmail.com>
2022-03-20 23:44:50 +0000

    Update profile.py

--- addiction_recovery/profile.py
+++ addiction_recovery/profile.py
@@ -1,1 +1,18 @@
+class profile:
+    def __init__(self, name, weight, height, age):
+        self.name=name
+        self.weight=weight
+        self.height=height
+        self.age=age
 
+    def update_name(self, newname):
+        self.name=newname
+
+    def update_weight(self, newweight):
+        self.weight=newweight
+
+    def update_height(self, newheight):
+        self.height=newheight
+
+    def update_name(self, newname):
+        self.newname=newname
