1a889c92614e5fe4c1516e44ace299a9ee7f1f7d
farazovo <farazovo@gmail.com>
2022-03-21 00:39:03 +0000

    Update profile.py

--- addiction_recovery/profile.py
+++ addiction_recovery/profile.py
@@ -1,9 +1,13 @@
+from datetime import date
 class profile:
-    def __init__(self, name, weight, height, age):
+    def __init__(self, name, weight, height, DOB):
         self.name=name
         self.weight=weight
         self.height=height
-        self.age=age
+        #DOB in (year, month, day) format
+        self.DOB=DOB
+        self.age=calculate_age(date(DOB))
+
 
     def update_name(self, newname):
         self.name=newname
@@ -14,5 +18,15 @@ class profile:
     def update_height(self, newheight):
         self.height=newheight
 
-    def update_name(self, newname):
-        self.newname=newname
+    def update_DOB(self, newDOB):
+        self.DOB=newDOB
+        self.age=calculate_age(date(newDOB))
+
+    def calculate_age(self, DOB):
+        yeardays = 365.2425   
+        age = int((date.today() - DOB).days / yeardays)
+        return age
+
+    def recacl_age(self):
+        self.age=calculate_age(date(self.DOB))
+
