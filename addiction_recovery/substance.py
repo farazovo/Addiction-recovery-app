4fa713a4c03d9af63b0b5a45f43d4f0ca7b4871b
farazovo <farazovo@gmail.com>
2022-03-21 03:44:59 +0000

    Update substance.py

--- addiction_recovery/substance.py
+++ addiction_recovery/substance.py
@@ -1,1 +1,12 @@
 
+class substance:
+    def __init__(self, name, halflife):
+        #e.g alchol, caffine
+        self.name=name
+        self.halflife=halflife
+
+    def return_name(self):
+        return self.name
+
+    def return_halflife(self):
+        return halflife
