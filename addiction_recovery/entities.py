c37768302ceb0998761a84b3ca2136562e7297fd
Joshua <80783216+Joshuii@users.noreply.github.com>
2022-03-24 09:03:25 +0000

    Create and retrieve entities

---
+++ addiction_recovery/entities.py
@@ -0,0 +1,87 @@
+from dataclasses import dataclass
+import datetime
+
+
+@dataclass
+class Person:
+    """
+    Properties:
+        names, weight, height, dob (date of birth as unix timestamp), id (automatically assigned)
+    """
+    name: str
+    weight: int
+    height: int
+    dob: int  # Unix timestamp
+    id: int = None
+
+    def calculate_age(self) -> int:
+        dob_datetime = datetime.datetime.fromtimestamp(self.dob)
+        return int((datetime.date.today() - dob_datetime).days / 365.2425)
+
+
+@dataclass
+class SubstanceTracking:
+    """
+    Properties:
+        person_id, substance_id, id (automatically assigned)
+    """
+    person_id: int
+    substance_id: int
+    id: int = None
+
+
+@dataclass
+class Substance:
+    """
+    Properties:
+        name, half_life, id (automatically assigned)
+    """
+    name: str
+    half_life: float
+    id: int = None
+
+
+@dataclass
+class SubstanceUse:
+    """
+    Properties:
+        substance_tracking_id, amount_id, time (unix timestamp), id (automatically assigned)
+    """
+    substance_tracking_id: int
+    amount_id: int
+    time: int  # Unix timestamp
+    id: int = None
+
+
+@dataclass
+class SubstanceAmount:
+    """
+    Properties:
+        amount, cost (in pence), id (automatically assigned)
+    """
+    amount: float
+    cost: int
+    id: int = None
+
+
+@dataclass
+class Goal:
+    """
+    Properties:
+        substance_tracking_id, goal_type_id, value, id (automatically assigned)
+    """
+    substance_tracking_id: int
+    goal_type_id: int
+    value: int
+    id: int = None
+
+
+@dataclass
+class GoalType:
+    """
+    Properties:
+        name, description, id (automatically assigned)
+    """
+    name: str
+    description: str
+    id: int = None
--- addiction_recovery/repository.py
+++ addiction_recovery/repository.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
 import sqlite3
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Tuple, Optional
+
+from entities import *
 
 
 class Repository(ABC):
@@ -21,6 +23,59 @@ class Repository(ABC):
     def close(self):
         if Repository.instance == self:
             Repository.instance = None
+
+    """
+    Create entities:
+        These methods create an entity and return their id
+    """
+
+    @abstractmethod
+    def create_person(self, person: Person) -> int: pass
+
+    @abstractmethod
+    def create_substance_tracking(self, tracking: SubstanceTracking) -> int: pass
+
+    @abstractmethod
+    def create_substance(self, substance: Substance) -> int: pass
+
+    @abstractmethod
+    def create_substance_use(self, use: SubstanceUse) -> int: pass
+
+    @abstractmethod
+    def create_substance_amount(self, amount: SubstanceAmount) -> int: pass
+
+    @abstractmethod
+    def create_goal(self, goal: Goal) -> int: pass
+
+    @abstractmethod
+    def create_goal_type(self, goal_type: GoalType) -> int: pass
+
+    """
+    Retrieve data:
+        These methods perform queries on the data. They either return the requested data
+        or None if data matching the query cannot be found.
+    """
+
+    @abstractmethod
+    def get_person(self, person_id: int) -> Optional[Person]: pass
+
+    @abstractmethod
+    def get_substance_tracking(self, tracking_id: int) -> Optional[SubstanceTracking]: pass
+
+    @abstractmethod
+    def get_substance(self, substance_id: int) -> Optional[Substance]: pass
+
+    @abstractmethod
+    def get_substance_use(self, use_id: int) -> Optional[SubstanceUse]: pass
+
+    @abstractmethod
+    def get_substance_amount(self, amount_id: int) -> Optional[SubstanceAmount]: pass
+
+    @abstractmethod
+    def get_goal(self, goal_id: int) -> Optional[Goal]: pass
+
+    @abstractmethod
+    def get_goal_type(self, goal_type_id: int) -> Optional[GoalType]: pass
 
 
 class SqlRepository(Repository):
@@ -56,7 +111,7 @@ class SqlRepository(Repository):
                     name TEXT,
                     weight INTEGER,
                     height INTEGER,
-                    age INTEGER
+                    dob INTEGER
                 );
             """)
             self.cursor.execute("""
@@ -150,7 +205,7 @@ class SqlRepository(Repository):
 
         :param query: the SQL query to be executed
         :param parameters: the parameters that are to be supplied to the SQL query
-        :return: A list of tuples that represent the rows that matched the query
+        :return: a list of tuples that represent the rows that matched the query
         """
         try:
             self.cursor.execute(query, parameters)
@@ -164,3 +219,108 @@ class SqlRepository(Repository):
                     with parameters '{parameters}' : {e.args} \033[0m"
                 )
             return []
+
+    """ Create entities """
+
+    def create_person(self, person: Person) -> int:
+        self.try_execute_command(
+            "INSERT INTO Person(name, weight, height, dob) VALUES (?, ?, ?, ?);",
+            (person.name, person.weight, person.height, person.dob)
+        )
+        return self.cursor.lastrowid
+
+    def create_substance_tracking(self, tracking: SubstanceTracking) -> int:
+        self.try_execute_command(
+            "INSERT INTO SubstanceTracking(person_id, substance_id) VALUES (?, ?);",
+            (tracking.person_id, tracking.substance_id)
+        )
+        return self.cursor.lastrowid
+
+    def create_substance(self, substance: Substance) -> int:
+        self.try_execute_command(
+            "INSERT INTO Substance(name, half_life) VALUES (?, ?);",
+            (substance.name, substance.half_life)
+        )
+        return self.cursor.lastrowid
+
+    def create_substance_use(self, use: SubstanceUse) -> int:
+        self.try_execute_command(
+            "INSERT INTO SubstanceUse(substance_tracking_id, amount_id, time) VALUES (?, ?, ?);",
+            (use.substance_tracking_id, use.amount_id, use.time)
+        )
+        return self.cursor.lastrowid
+
+    def create_substance_amount(self, amount: SubstanceAmount) -> int:
+        self.try_execute_command(
+            "INSERT INTO SubstanceAmount(amount, cost) VALUES (?, ?);",
+            (amount.amount, amount.cost)
+        )
+        return self.cursor.lastrowid
+
+    def create_goal(self, goal: Goal) -> int:
+        self.try_execute_command(
+            "INSERT INTO Goal(substance_tracking_id, goal_type_id, value) VALUES (?, ?, ?);",
+            (goal.substance_tracking_id, goal.goal_type_id, goal.value)
+        )
+        return self.cursor.lastrowid
+
+    def create_goal_type(self, goal_type: GoalType) -> int:
+        self.try_execute_command(
+            "INSERT INTO GoalType(name, description) VALUES (?, ?);",
+            (goal_type.name, goal_type.description)
+        )
+        return self.cursor.lastrowid
+
+    """ Retrieve data """
+
+    def get_entity(self, table: str, entity_id: int) -> Optional[Tuple]:
+        """
+        Helper function to query the database to find an entity.
+
+        :param table: the name of the table in the database to be queried
+        :param entity_id: the id of the entity to be retrieved
+        :return: a tuple that represents the entity that matched the query
+        """
+        entities = self.try_execute_query(
+            f"SELECT * FROM {''.join(filter(lambda c: c.isalpha(), table))} WHERE id = ?",
+            (entity_id,)
+        )
+        if len(entities):
+            # Unpack and reorder tuple to put id last
+            return *entities[0][1:], entities[0][0]
+        return None
+
+    def get_person(self, person_id: int) -> Optional[Person]:
+        if data := self.get_entity("Person", person_id):
+            return Person(*data)
+        return None
+
+    def get_substance_tracking(self, tracking_id: int) -> Optional[SubstanceTracking]:
+        if data := self.get_entity("SubstanceTracking", tracking_id):
+            return SubstanceTracking(*data)
+        return None
+
+    def get_substance(self, substance_id: int) -> Optional[Substance]:
+        if data := self.get_entity("Substance", substance_id):
+            return Substance(*data)
+        return None
+
+    def get_substance_use(self, use_id: int) -> Optional[SubstanceUse]:
+        if data := self.get_entity("SubstanceUse", use_id):
+            return SubstanceUse(*data)
+        return None
+
+    def get_substance_amount(self, amount_id: int) -> Optional[SubstanceAmount]:
+        if data := self.get_entity("SubstanceAmount", amount_id):
+            return SubstanceAmount(*data)
+        return None
+
+    def get_goal(self, goal_id: int) -> Optional[Goal]:
+        if data := self.get_entity("Goal", goal_id):
+            return Goal(*data)
+        return None
+
+    def get_goal_type(self, goal_type_id: int) -> Optional[GoalType]:
+        if data := self.get_entity("GoalType", goal_type_id):
+            return GoalType(*data)
+        return None
